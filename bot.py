ae7168cd6e2aba61bbbc33d38d4b80d9680daa1f
Miro Nieminen <johannes.nieminen@hotmail.com>
2011-12-29 23:59:47 +0200

    Iso update

--- bot.py
+++ bot.py
@@ -1,24 +1,58 @@
+#-*- coding: UTF-8 -*-
 import connection
 import oraakkeli
 import time
 
-c = connection.Connection()
-o = oraakkeli.Oraakkeli()
-c.startChat()
-c.randomChat()	
-print "outo: "+c.getUserId()
-print "sina: "+c.getStrangerId()
+class Botti:
+	def __init__(self):
+		self.chat = connection.Connection()
+		self.oraakkeli = oraakkeli.Oraakkeli()
 
-c.sendMsg("moro")
-c.listenToReceive()
-while(c.getReceiveMsg() != "||--noResult--||"):
-	c.listenToReceive()
-	if (c.getReceiveMsg() != ""):
-		print ">>: "+c.getReceiveMsg()
-		oraakkeliVastaus = o.getMsg(c.getReceiveMsg())
-		time.sleep(1)
-		c.sendMsg(oraakkeliVastaus)
-		print "<<: "+oraakkeliVastaus
-	else:
-		time.sleep(1)
+	def initChat(self):
+		self.chat.setUserId("0")
+		self.chat.setStrangerId("0")
 
+		print "Get userId..."
+		while(self.chat.getUserId() == "0"):
+			self.chat.startChat()
+			time.sleep(1)
+		print "UserId: "+str(self.chat.getUserId())
+
+		print "Get strangerId..."
+		while(self.chat.getStrangerId() == "0"):
+			self.chat.randomChat()
+			time.sleep(1)
+
+		print "StrangerId: "+str(self.chat.getStrangerId())
+		self.chat.receiveMsg = ""
+
+		self.startTime = int(time.clock())
+
+	def run(self):
+		while(True):
+			self.chat.listenToReceive()
+			msg = self.chat.getReceiveMsg()
+			msg = msg.replace("&auml;", "ä")
+			msg = msg.replace("&ouml;", "ö")
+			msg = msg.replace("&Auml;", "Ä")
+			msg = msg.replace("&Ouml;", "Ö")
+			if(msg == "||--noResult--||"):
+				print "Stranger leaved"
+				self.initChat()
+			elif (msg != ""):
+				responseMsg = self.oraakkeli.getMsg(msg)
+				self.chat.sendMsg(responseMsg)
+				print "Stranger: "+msg
+				print "You: "+responseMsg
+
+			if self.startTime+60 < int(time.clock()):
+				print "Get new stranger"
+				self.initChat()
+
+			time.sleep(1)
+				
+
+if __name__ == "__main__":
+	b = Botti()
+	b.initChat()
+	b.run()
